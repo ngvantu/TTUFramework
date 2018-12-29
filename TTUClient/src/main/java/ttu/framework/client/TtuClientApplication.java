7eb486ce4a766506200aec936f2ccfe2af808eeb
nguyenngocthai <thainguyenngocnbk@gmail.com>
2018-12-29 09:04:15 +0700

    init ttuclient add resource ttuframework

---
+++ TTUClient/src/main/java/ttu/framework/client/TtuClientApplication.java
@@ -0,0 +1,14 @@
+package ttu.framework.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtuClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TtuClientApplication.class, args);
+	}
+
+}
+
